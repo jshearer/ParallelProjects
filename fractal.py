1f7e1359c0c18cd4acf14fcb27ed5f76453d4b8c
Joseph Shearer <jshearer@K20cGPU.(none)>
2013-10-27 15:33:22 -0400

    Changed some parameter setup so zooming is less painful.

--- fractal.py
+++ fractal.py
@@ -9,6 +9,7 @@ from Vector import Vector
 import sys
 import time
 import traceback
+import math
 
 import pycuda.driver as cuda
 import pycuda.autoinit
@@ -16,7 +17,7 @@ from pycuda.compiler import SourceModule
 
 genChunk = SourceModule("""
 #include <cuComplex.h>
-__global__ void gen(int size[2],int position[2],int realBlockDim[2],float *zoom,int *iterations,int *result)
+__global__ void gen(int size[2],float position[2],int realBlockDim[2],float *zoom,int *iterations,int *result)
 {	
 	int startx = blockIdx.x*size[0];
 	int starty = blockIdx.y*size[1];
@@ -40,27 +41,28 @@ __global__ void gen(int size[2],int position[2],int realBlockDim[2],float *zoom,
 		}
 	}
 }
-//result width = 
+
 """).get_function("gen")
 print("Compiled and got function gen")
 #Variable setup section
-block = (10,10,1) #number of GPU cores launched is block[0]*block[1]
-size = numpy.array([600,600],dtype=numpy.int32) #This is the chunk size each core is to calculate, in parallel
-res_shape = (size[0]*block[0],size[1]*block[1]) #Shape for the output array, that is passed into the kernel
-result = numpy.zeros(res_shape,dtype=numpy.int32) #Actual output array that is passed into the kernel, and then read once generation is complete
-zoom = numpy.float32(4000) #higher number = more zoomed in. Supposed to zoom into the center, scaling around position.
-iterations = numpy.int32(100) #max number of iterations. Make higher for more pretty
+block = (40,40,1) #number of GPU cores launched is block[0]*block[1]
+size = numpy.array([int(math.ceil(350.0/float(block[0]))),int(math.ceil(350.0/float(block[1])))],dtype=numpy.int32) #This is the chunk size each core is to calculate, in parallel
+zoom = numpy.float32(270) #higher number = more zoomed in. Supposed to zoom into the center, scaling around position.
+iterations = numpy.int32(600) #max number of iterations. Make higher for more pretty
 
-position = numpy.array([(block[0]*size[0])/2,(block[1]*size[1])/2],dtype=numpy.int32) #center fractal on middle of generated image.
-position = numpy.array([-7000,0],dtype=numpy.int32) #change this to offset fractal
+position = numpy.array([-1,0],dtype=numpy.float32) #change this to offset fractal
 
 #this math makes it so when you increase zoom, it acts like you would expect. Otherwise it would zoom around the origin, which no one wants.
-scale = 1.0 #if you do, just comment this out
-zoom = numpy.float32(zoom/scale)
-position = Vector(position[0]/scale,position[1]/scale)
-position = position - (Vector(result.shape[0],result.shape[1])/2)
-position = numpy.array([int(position.x),int(position.y)]).astype(numpy.int32)
+scale = 10.0 #if you do, just comment this out
 
+size = (size.astype(numpy.float32)*float(scale)).astype(numpy.int32)
+res_shape = (size[0]*block[0],size[1]*block[1]) #Shape for the output array, that is passed into the kernel
+result = numpy.zeros(res_shape,dtype=numpy.int32) #Actual output array that is passed into the kernel, and then read once generation is complete
+
+zoom = numpy.float32(zoom*scale)
+position = Vector(position[0]*zoom,position[1]*zoom)
+position = position - (Vector(result.shape[0],result.shape[1])/2)
+position = numpy.array([int(position.x),int(position.y)]).astype(numpy.float32)
 
 print("Setup arguments, running now, size: "+str(size)+", zoom: "+str(zoom)+", iterations: "+str(iterations)+", position: "+str(position)+", result shape: ("+str(result.shape)+", "+str(res_shape)+"), result size: "+str((result.nbytes*8)/32))
 
@@ -71,11 +73,12 @@ cuda.Context.synchronize()
 print("Done running. synched.")
 print(result)
 
-result2 = numpy.zeros((result.shape[1],result.shape[0],3)).astype(numpy.uint8)
+print("Resizing result to be in range 0-255")
 result = result.astype(numpy.float32)*(255.0/iterations)
-for x in range(0,result2.shape[0]-1):
-	for y in range(0,result2.shape[1]-1):
-		result2[x,y,0] = result[y,x]
-		result2[x,y,1] = result[y,x]
-		result2[x,y,2] = result[y,x]
+print("Done resizing. Now generating image array.")
+
+result2 = numpy.hstack((result,result,result))
+result2.resize((result.shape[0],result.shape[1],3))
+result2 = result2.astype(numpy.uint8)
+
 Image.fromarray(result2,"RGB").save("testfractal.png")
