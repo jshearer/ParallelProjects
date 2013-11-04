4ac20350561c636dc28fecfb77f8da7acd5ad385
Joseph Shearer <jshearer@K20cGPU.(none)>
2013-11-04 14:58:46 -0500

    Cleaned up fractal generation into a nice function. Added an easy gif generator for animated fractal renders.

--- fractal.py
+++ fractal.py
@@ -10,6 +10,7 @@ import sys
 import time
 import traceback
 import math
+import time
 
 import pycuda.driver as cuda
 import pycuda.autoinit
@@ -44,39 +45,37 @@ __global__ void gen(int size[2],float position[2],int realBlockDim[2],float *zoo
 
 """).get_function("gen")
 print("Compiled and got function gen")
-#Variable setup section
-block = (40,40,1) #number of GPU cores launched is block[0]*block[1]
-size = numpy.array([int(math.ceil(2000.0/float(block[0]))),int(math.ceil(1000.0/float(block[1])))],dtype=numpy.int32) #This is the chunk size each core is to calculate, in parallel
-zoom = numpy.float32(4000) #higher number = more zoomed in. Supposed to zoom into the center, scaling around position.
-iterations = numpy.int32(17) #max number of iterations. Make higher for more pretty
 
-position = numpy.array([-1.45,0],dtype=numpy.float32) #change this to offset fractal
+def GenerateFractal(dimensions,position,zoom,iterations,block=(20,20,1)):
+	chunkSize = numpy.array([dimensions[0]/block[0],dimensions[1]/block[1]],dtype=numpy.int32)
+	zoom = numpy.float32(zoom)
+	iterations = numpy.int32(iterations)
+	blockDim = numpy.array([block[0],block[1]],dtype=numpy.int32)
+	result = numpy.zeros(dimensions,dtype=numpy.int32)
 
-#this math makes it so when you increase zoom, it acts like you would expect. Otherwise it would zoom around the origin, which no one wants.
-scale = 1.0 #if you do, just comment this out
+	#Center position
+	position = Vector(position[0]*zoom,position[1]*zoom)
+	position = position - (Vector(result.shape[0],result.shape[1])/2)
+	position = numpy.array([int(position.x),int(position.y)]).astype(numpy.float32)
 
-size = (size.astype(numpy.float32)*float(scale)).astype(numpy.int32)
-res_shape = (size[0]*block[0],size[1]*block[1]) #Shape for the output array, that is passed into the kernel
-result = numpy.zeros(res_shape,dtype=numpy.int32) #Actual output array that is passed into the kernel, and then read once generation is complete
+	print("Calling CUDA function. Starting timer.")
+	start_time = time.time()
 
-zoom = numpy.float32(zoom*scale)
-position = Vector(position[0]*zoom,position[1]*zoom)
-position = position - (Vector(result.shape[0],result.shape[1])/2)
-position = numpy.array([int(position.x),int(position.y)]).astype(numpy.float32)
+	genChunk(cuda.In(chunkSize), cuda.In(position), cuda.In(blockDim), cuda.In(zoom), cuda.In(iterations), cuda.InOut(result), block=(1,1,1), grid=block)
+	end_time = time.time()
+	elapsed_time = end_time-start_time
+	print("Done with call. Took "+str(elapsed_time)+" seconds. Here's the repr'd arary:\n")
+	result[result.shape[0]/2,result.shape[1]/2]=iterations+1
+	print(result)
+	return result
 
-print("Setup arguments, running now, size: "+str(size)+", zoom: "+str(zoom)+", iterations: "+str(iterations)+", position: "+str(position)+", result shape: ("+str(result.shape)+", "+str(res_shape)+"), result size: "+str((result.nbytes*8)/32))
+def SaveToPng(result,name):
+	print("Resizing result to be in range 0-255")
+	result = (result.astype(numpy.float32)*(255.0/result.max())).astype(numpy.uint8)
+	print("Done resizing. Now generating image array.")
 
-genChunk(cuda.In(size),cuda.In(position),cuda.In(numpy.array([block[0],block[1]],dtype=numpy.int32)),cuda.In(zoom),cuda.In(iterations),cuda.InOut(result),block=(1,1,1),grid=block)
-print("Executed.")
-#call using cuda.In, cuda.Out etc
-cuda.Context.synchronize()
-print("Done running. synched.")
-print(result)
+	result = result.reshape((result.shape[1],result.shape[0]))
+	print("Done generating image array. Writing image file.")
 
-print("Resizing result to be in range 0-255")
-result = (result.astype(numpy.float32)*(255.0/result.max())).astype(numpy.uint8)
-print("Done resizing. Now generating image array.")
-
-result = result.reshape((result.shape[1],result.shape[0]))
-
-Image.fromstring("L",(result.shape[1],result.shape[0]),result.tostring()).save("testfractal.png")
+	Image.fromstring("L",(result.shape[1],result.shape[0]),result.tostring()).save(name+".png")
+	print("Image file written.")
