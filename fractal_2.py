b936f0796b17d1d1262351921a8513b33146753d
Joseph Shearer <jshearer@K20cGPU.(none)>
2013-12-28 21:35:17 -0500

    Added many comments, made code easier to read. Yay for readability! More work to do.

--- fractal_2.py
+++ fractal_2.py
@@ -35,7 +35,7 @@ def genChunk(position,size,zoom,iterations):
 
 		return (result,position,size)
 	except Exception as e:
-		print "ERROR::::::::: "+str(e)
+		print("ERROR::::::::: "+str(e))
 		traceback.print_exc(file=sys.stdout)
 
 #position,zoom,resolution,scale
@@ -44,7 +44,8 @@ def genChunk(position,size,zoom,iterations):
 #dimensions is the size, as a 2-tuple, of the output image
 #scale is a number that allows you to reduce the zoom and dimensions simultaneously 
 #in order to render a lower-res version of the same fractal
-def gen(position,zoom,dimensions,name,scale=1,squaresize=50,processes=4,scd=False,silent=False, iterations=100): #scale change dimensions
+
+def gen(position,zoom,dimensions,name,scale=1,squaresize=50,processes=4,silent=False, iterations=100):
 	scale = 1.0/scale
 	procPool = Pool(processes=processes)
 
@@ -52,8 +53,7 @@ def gen(position,zoom,dimensions,name,scale=1,squaresize=50,processes=4,scd=Fals
 	position.reverse()
 	dimensions.reverse()
 	zoom = zoom/scale
-	if scd:
-		dimensions = (int(dimensions[0]/scale),int(dimensions[1]/scale))
+	dimensions = (int(dimensions[0]/scale),int(dimensions[1]/scale))
 
 	position = Vector(position[0]/scale,position[1]/scale)
 	position = position - (Vector(dimensions[0],dimensions[1])/2)
@@ -61,50 +61,57 @@ def gen(position,zoom,dimensions,name,scale=1,squaresize=50,processes=4,scd=Fals
 
 	startime = time.time()
 
-	grad = {-999999999999:(0,0,0,255), #Color data
-				3:(255,0,0,255),
-				2:(255,0,100,255),
-				1:(255,0,200,255),
-				4:(255,0,255,255),
-				6:(100,100,100,255),
-				10:(100,100,100,255),
-				50:(0,0,0,255),
+	grad = {	0:(0,0,0,255), #Color data
+				50:(255,100,255,255),
 				100:(255,100,200,255),
 				99999999:(255,255,255,255)}
-	result = result = numpy.zeros((dimensions[0],dimensions[1],3),dtype=numpy.uint8)
+	
+	result = numpy.zeros((dimensions[0],dimensions[1],3),dtype=numpy.uint8)
+	
 	lookup = []
-	for i in range(0,200):
-		lookup.append(utils.getGradCol(i,grad))
-	num = 1
-	done = [0]
+	for i in range(0,255):
+		lookup.append(utils.getGradCol(i,grad)) #precache gradient values in order to avoid repetitive function calls
+
+	num_completed = {'done':0,'total':0} #This hack is nessecary because python 2 doesn't have the nonlocal keyword, so callback() couldn't modify num_completed if it were just an int.
+						#Apparently, single element arrays work as a replacement...
+
 	def callback(data): #should find a better numpy-way to do this...
+
+		#data is the return of genChunk, which is:
+		#return (result,position,size)
+
 		pos = (data[1][0]-position[0],data[1][1]-position[1])
 		size = data[2]
-		data = data[0] #overwrite
-		if not silent:
-			pct = int((done[0]/float(num))*100.0*0.3)
-			sys.stdout.write("\r["+("#"*pct)+(" "*(30-pct))+"] "+str(int((done[0]/float(num))*100.0))+"% <"+str(int(time.time()-startime))+"s>")
-		dat = 0
-		done[0] += 1
+		subgrid = data[0]
+
+		num_completed['done'] = num_completed['done'] + 1
+
+		if not silent: #report progress to stdout, using FancyProgressBar(tm)
+			pct = int((num_completed['done']/float(num_completed['total']))*100.0*0.3)
+			sys.stdout.write("\r["+("#"*pct)+(" "*(30-pct))+"] "+str(int((num_completed['done']/float(num_completed['total']))*100.0))+"% <"+str(int(time.time()-startime))+"s> ("+str(num_completed['done'])+")")
+
 		try:
 			for x in range(pos[0],size[0]+pos[0]):
 				for y in range(pos[1],size[1]+pos[1]):
-					dat = min(int(data[x-pos[0],y-pos[1]]),100)
-					result[x,y,0] = lookup[dat][0]
-					result[x,y,1] = lookup[dat][1]
-					result[x,y,2] = lookup[dat][2]
+					clamped = min(int(subgrid[x-pos[0],y-pos[1]]),100)
+					result[x,y,0] = lookup[clamped][0] #R
+					result[x,y,1] = lookup[clamped][1] #G
+					result[x,y,2] = lookup[clamped][2] #B
 		except Exception as e:
 			if not silent:
-				print "Error:::::: "+str(e)+str(dat)
+				print("Error:::::: "+str(e))
+
 
 	for x in xrange(position[0],dimensions[0]+position[0],squaresize):
 		for y in xrange(position[1],dimensions[1]+position[1],squaresize):
 			procPool.apply_async(genChunk,[(x,y),(squaresize,squaresize),zoom,iterations],callback=callback,)
-			num += 1
-	num -=1
+			num_completed['total'] += 1
 
 	procPool.close()
 	procPool.join()
+
+	result = numpy.rot90(result)
+
 	Image.fromarray(result,"RGB").save(name)
 
 
@@ -125,11 +132,10 @@ parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpForm
 parser.add_argument('--position','-p', metavar="pos", nargs=2, type=float, required=True, help="The offset of the rendered fractal. If set to 0 0, the fractal will be centered in the output image.")
 parser.add_argument('--dimensions','-d', metavar="dim", nargs=2, type=int, required=True, help="The rendered image dimensions, in pixels. This may be modified by the use of the scale argument.")
 parser.add_argument('--zoom','-z', type=float, required=True, help="The zoom of the fractal. This may be modified by the scale argument.")
-parser.add_argument('--scale','-s', type=float, default=1, help="The scale multiplier. 1=default, use provided position and zoom. 2=2x zoom and position. If used with the -m argument, the output dimensions will also be scaled up. This can be used to make a thumbnail image before running the full render, to see if your coordinates are correct.\n")
-parser.add_argument('--processes','-procs', type=int, default=4, help="The number of processes to use for multi-process rendering. This is usually the number of CPU cores you have.")
-parser.add_argument('-m', dest="scd", action="store_true", help="If this flag is set, then the scale argument will also modify the dimensions of the image.")
+parser.add_argument('--scale','-s', type=float, default=1, help="The scale multiplier. 1=default, use provided position and zoom. 2=2x zoom and position. This can be used to make a thumbnail image before running the full render, to see if your coordinates are correct.\n")
+parser.add_argument('--processes','-procs', type=int, default=4, help="The number of processes to use for multi-process rendering. This is usually the number of CPU cores you have. Defaults to 4")
 parser.add_argument('--iterations','-i', dest="iters", default=50, type=int, help="The maximum number of iterations the generator will go through, per pixel. The higher this is, the more accurate the fractal will be, and the slower the generation will be. It will be especially slow in places where the pixel is inside the shape, in which case the loop would go on for ever without a limit.")
-parser.add_argument('--calculate', action="store_true", dest="calc", help="If this flag is set, no fractal will be rendered. Instead, scale will be applied, and the resulting values will be printed out, so that you can use them without using scale. (Work on this description...)")
+parser.add_argument('--calculate', action="store_true", dest="calc", help="If this flag is set, no fractal will be rendered. Instead, the parameters will be outputed to stdout, calculated such that they can be applied with scale at 1. ")
 parser.add_argument('--blocksize', dest="bsize", type=int, default=50, help="Set the block generation size. Default 50.")
 parser.add_argument('name', help="The name of the output file. This can have any image extension supported by PIL.")
 args = vars(parser.parse_args())
@@ -137,7 +143,6 @@ args = vars(parser.parse_args())
 if args['calc']:
 	position = args['position']
 	zoom = args['zoom']
-	scd = args['scd']
 	scale = 1.0/args['scale']
 	dimensions = args['dimensions']
 	
@@ -145,17 +150,14 @@ if args['calc']:
 	position.reverse()
 	
 	zoom = zoom/scale
-	if scd:
-		dimensions = (int(dimensions[0]/scale),int(dimensions[1]/scale))
+	dimensions = (int(dimensions[0]/scale),int(dimensions[1]/scale))
 
 	position = Vector(position[0]/scale,position[1]/scale)
 	position = (int(position.x),int(position.y))
-	print "./fractal.py -p "+str(position[1])+" "+str(position[0])+" -d "+str(dimensions[1])+" "+str(dimensions[0])+" -z "+str(zoom)
+	print("./fractal.py -p "+str(position[1])+" "+str(position[0])+" -d "+str(dimensions[1])+" "+str(dimensions[0])+" -z "+str(zoom))
 else:
 	try:
-		os.system("setterm -cursor off")
-		gen(args['position'],args['zoom'],args['dimensions'],args['name'], processes=args['processes'], scd=args['scd'], scale=args['scale'], iterations=args['iters'], squaresize=args['bsize'])
-		os.system("setterm -cursor on")
+		gen(args['position'],args['zoom'],args['dimensions'],args['name'], processes=args['processes'], scale=args['scale'], iterations=args['iters'], squaresize=args['bsize'])
 	except Exception as exc:
 		os.system("setterm -cursor on")
 		traceback.print_exc(file=sys.stdout)
