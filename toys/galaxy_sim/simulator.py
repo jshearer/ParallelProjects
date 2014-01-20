e9a648cfc9fa31cff8af180c7b7bf894d3418e5a
Joseph Shearer <joseph@josephshearer.net>
2014-01-20 01:31:48 -0500

    Write version 0.1 of simulation code.

--- toys/galaxy_sim/simulator.py
+++ toys/galaxy_sim/simulator.py
@@ -3,9 +3,12 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
 CUDASimulate = SourceModule("""
-#DEFINE .x [0]
-#DEFINE .y [1]
-#DEFINE .z [2]
+#define .x [0]
+#define .y [1]
+#define .z [2]
+#define getMass(id) stars[id+6]
+#define getPosition(star_id) vec3fCreate(stars[star_id],stars[star_id+1],stars[star_id+2])
+#define getVelocity(star_id) vec3fCreate(stars[star_id+3],stars[star_id+4],stars[star_id+5])
 
 __device__ float* vec3fCreate(float x, float y, float z)
 {
@@ -72,10 +75,62 @@ __device__ void vec3fNormalize(float* vec)
 	free(div);
 }
 
-__global__ void sim(float* stars, int numstars, int stride)
-{	
-	for(int star_id = blockIdx.x; star_id<numstars; star_id += (stride){
+__global__ void sim(float* stars, int numstars, int stride, float timestep)
+{
+	float* NewVelocity, NewPosition;
+	float* stepVector = vec3fCreate(timestep,timestep,timestep);
+	float mass,len,force;
+	for(int star_id = blockIdx.x; star_id<numstars; star_id += stride*7){
 
+		NewVelocity = getVelocity(star_id);
+
+		for(int that_id = 0; that_id<numstars; that_id+=7)
+		{
+			if(that_id != star_id)
+			{
+				float* thisPosition = getPosition(star_id);
+				float* thatPosition = getPosition(that_id);
+				float* thisMinusThat = float[3];
+				vec3fSub(thisPosition,thatPosition,thisMinusThat);
+
+				//Calculate force magnitude
+				mass = getMass(star_id)*getMass(that_id);
+				len = vec3fLen(thisMinusThat);
+				force = mass/powf(len,2);
+				float* forceVec = vec3fCreate(force,force,force);
+
+				vec3fNormalize(thisMinusThat); //Calculate direction of influence
+				vec3fMul(thisMinusThat,forceVec,thisMinusThat); //multiply by force magnitude
+				vec3fMul(thisMinusThat,stepVector,thisMinusThat);
+
+				vec3fAdd(NewVelocity,thisMinusThat,NewVelocity)
+
+				free(thisPosition);
+				free(thatPosition);
+				free(forceVec);
+				free(thisMinusThat);
+			}
+		}
+
+		//I would like to wait until *all* blocks have reached this point, but that's not possible in CUDA. Must find a better way. Currenly with no synchronization scheme in place, I would expect to see a slow but steady declie in accuracy as a certain percentage of blocks finish looping sooner than others, thus getting slightly ahead. Though possibly they will then get slightly behind, thus canceling everything out. Accuracey wise, this is bad. Performance wise, this is good.
+
+		//store NewVelocity
+		stars[star_id+3] = NewVelocity.x
+		stars[star_id+4] = NewVelocity.y
+		stars[star_id+5] = NewVelocity.z
+
+		NewPosition = getPosition(star_id);
+		vec3fAdd(NewPosition,NewVelocity,NewPosition);
+
+		//store NewPosition
+		stars[star_id] = NewPosition.x
+		stars[star_id+1] = NewPosition.y
+		stars[star_id+2] = NewPosition.z
+
+		free(NewPosition);
+		free(NewVelocity);
+
+		
 	}
 
 	//Make sure every thread is done calculating before going on to the next timestep.
