fbd30b798bddd0f9b51676f937184c859e1afcf9
Joseph Shearer <joseph@josephshearer.net>
2014-02-20 16:44:00 -0500

    Fixed potential problem with overlap and log scales.

--- fractal/plot_data.py
+++ fractal/plot_data.py
@@ -6,7 +6,7 @@ import matplotlib as pltlib
 import time
 import numpy as np
 
-def makePlot(data,directory,xlog=True,ylog=False):
+def makePlot(data,directory,xlog=True,ylog=False,ovlog=False):
 	cores      = data[0]
 	times      = data[1]
 	threads    = data[2]
@@ -73,6 +73,8 @@ def makePlot(data,directory,xlog=True,ylog=False):
 		ax.set_xscale('log')
 	if ylog:
 		ax.set_yscale('log')
+	if ovlog: #overlap log
+		ax2.set_yscale('log')
 
 	ax.grid(True)
 
