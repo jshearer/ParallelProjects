b37a7b2acc7b9fedb98c52fad2787ffa2972cf20
Les Schaffer <les@designspring.com>
2014-03-07 09:19:57 -0500

    some formatting of test output of genParameters. added useOrigChk kwd arg to genParameters to test Yosef vs Les style tile checking. need to check discrepancy between the two outputs. new and old style checking captured in results folder.

--- fractal/utils.py
+++ fractal/utils.py
@@ -5,57 +5,64 @@ K20C_BLOCKS = 2048
 K20C_THREADS = 1024
 
 def getFactors(val):
-	return np.array([(i, val / i) for i in range(1, int(val+1)) if val % i == 0])
+    return np.array([(i, val / i) for i in range(1, int(val+1)) if val % i == 0])
 
-def genParameters(blocks,threads,dimensions,silent=False):
-	# should check that blocks <= blocks available on GPU
-	# should check that threads <= threads available on block on GPU
-	if blocks > K20C_BLOCKS:
-		raise ValueError("GPU does not have enough blocks")
-	if threads > K20C_THREADS:
-		raise ValueError("GPU does not have enough threads per block")
+def genParameters(blocks,threads,dimensions,silent=False, useOrigChk=True):
+    # should check that blocks <= blocks available on GPU
+    # should check that threads <= threads available on block on GPU
+    if blocks > K20C_BLOCKS:
+        raise ValueError("GPU does not have enough blocks")
+    if threads > K20C_THREADS:
+        raise ValueError("GPU does not have enough threads per block")
+
+    dimensions = np.array(dimensions)
+    px_total_needed =  dimensions.prod()
 	
-	dimensions = np.array(dimensions)
-	px_total_needed =  dimensions.prod()
-	
-	px_per_thread = px_total_needed / (blocks*threads)
-	px_total = px_per_thread  * blocks * threads
-	if px_total != px_total_needed:
-		if not silent:
-			print "WARNING: Current configuration of blocks and threads results in ",
-			print str(px_total) + "/" + str(px_total_needed) + " pixels calculated."
-		raise ValueError("Total pixels does not divide evenly across total threads.")
+    px_per_thread = px_total_needed / (blocks*threads)
+    px_total = px_per_thread  * blocks * threads
+    if px_total != px_total_needed:
+        if not silent:
+            print "WARNING: Current configuration of blocks and threads results in ",
+            print str(px_total) + "/" + str(px_total_needed) + " pixels calculated."
+        raise ValueError("Total pixels does not divide evenly across total threads.")
     
 	# we might want to try a shape in the middle of each first
-	for bestBlocks in getFactors(blocks):
-		px_per_block = dimensions/bestBlocks
-		dimensions_x = (px_per_block * bestBlocks)
-		#print 'dim: ', px_per_block, bestBlocks, dimensions, dimensions_x
-		if (1-(px_per_block!=0).astype(np.int)).sum()>=1:
-		#byteme
-		#if (dimensions_x != dimensions).prod():
-			if 0:
-				print dimensions, bestBlocks
-				print 'px_per_block = %s'%px_per_block
-				print 'px_per_block!=0 = %s'%(px_per_block!=0).astype(np.int)
-				print 'b4 sum = %s'%(1-(px_per_block!=0).astype(np.int))
-			continue
+    for bestBlocks in getFactors(blocks):
+        px_per_block = dimensions/bestBlocks
+        dimensions_x = (px_per_block * bestBlocks)
+        
+        if useOrigChk:
+            chk =  (1-(px_per_block!=0).astype(np.int)).sum()<1
+        else:
+            chk = (dimensions_x == dimensions).prod()
 
-		for bestThreads in getFactors(threads):
-			px_per_thread = px_per_block/bestThreads
-			px_per_block_x = px_per_thread * bestThreads
-			#print 'thread = ', px_per_block, px_per_block_x
-			if (1-(px_per_thread!=0).astype(np.int)).sum() < 1: # ==0???, anyway, we found a case, use it!
-			#if (px_per_block_x == px_per_block).prod():
-				if not silent:
-					print "Block dimensions are " + str(bestBlocks)+", each block contains " + str(px_per_block)+" pixels."
-					print "Thread dimensions per block are " + str(bestThreads)
-					print "Each thread will calculate " + str(px_per_thread) + " pixels."
-					print "Total number of pixels calculated: " + str(px_total)
-					print "Total number of threads to be launched " + str(bestBlocks*bestThreads),
-					print " = " + str((bestBlocks*bestThreads).prod()) + " total threads."
+        if not chk:
+            if 0:
+                print dimensions, bestBlocks
+                print 'px_per_block = %s'%px_per_block
+                print 'px_per_block!=0 = %s'%(px_per_block!=0).astype(np.int)
+                print 'b4 sum = %s'%(1-(px_per_block!=0).astype(np.int))
+            continue
+
+        for bestThreads in getFactors(threads):
+            px_per_thread = px_per_block/bestThreads
+            px_per_block_x = px_per_thread * bestThreads
+            
+            if useOrigChk:
+                chk = (1-(px_per_thread!=0).astype(np.int)).sum() < 1
+            else:
+                chk = (px_per_block_x == px_per_block).prod()
+
+            if chk:
+                if not silent:
+                    print "Block dimensions are " + str(bestBlocks)+", each block contains " + str(px_per_block)+" pixels."
+                    print "Thread dimensions per block are " + str(bestThreads)
+                    print "Each thread will calculate " + str(px_per_thread) + " pixels."
+                    print "Total number of pixels calculated: " + str(px_total)
+                    print "Total number of threads to be launched " + str(bestBlocks*bestThreads),
+                    print " = " + str((bestBlocks*bestThreads).prod()) + " total threads."
 					
-				return (bestBlocks,bestThreads,px_per_block,px_per_thread)
+                return (bestBlocks,bestThreads,px_per_block,px_per_thread)
 			
 	if not silent:
 		print "Combination of blocks, threads, and dimensions does not result in any block, ",
@@ -93,11 +100,16 @@ def bestPerim(val,ratio): #Was an idea, doesn't quite fit yet.
 	return np.array(best)
 
 if __name__ == '__main__':
-	
-	for blocks in range(1,2049):
-		for threads in range(1,1025):
-			try:
-				ps = genParameters(blocks, threads, [2000,1000], silent=True)
-				print '%d, %d, %s'%(blocks, threads, ps)
-			except ValueError:
-				pass
+    from string import join as sjoin
+    print ' -----------------------------------------------------------------------'
+    print ' |   blk,thd:: |    bestBlk  |  px_per_blk |    bstThd   |  px_per_thd | '
+    print ' -----------------------------------------------------------------------'
+    for blocks in range(1,2049):
+        for threads in range(1,1025):
+            try:
+                ps = genParameters(blocks, threads, [2000,1000], silent=True, useOrigChk=False)
+                ps = (ps[0], ps[2], ps[1], ps[3])
+                psOut = [ '%4d x %4d'%(arr[0], arr[1]) for arr in ps ]
+                print ' |%4d, %4d:: | %s |'%(blocks, threads, sjoin(psOut, ' | '))
+            except ValueError:
+                pass
