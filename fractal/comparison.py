d84bbe43f0b870324d2c084d50817c705452848a
Les Schaffer <les@designspring.com>
2014-03-08 14:09:55 -0500

    untabify. check if DISPLAY is set, if so, use WXAgg instead of Agg, and display plot on screen if show is True. use _VERBOSE to decide on prints.... do plt.show() AFTER plt.savefig, otherwise get blank png.

--- fractal/comparison.py
+++ fractal/comparison.py
@@ -3,30 +3,32 @@ import fractal_data
 import plot_data
 
 def compareParams(position, zoom, dimensions, name, iterations=100,save=True):
-	print "Comparing paramaters for ("+str(name)+"): ( ("+str(position[0])+", "+str(position[1])+"), "+str(zoom)+", ("+str(dimensions[0])+", "+str(dimensions[1])+") )"
+    print "Comparing paramaters for ("+str(name)+"): ( ("+str(position[0])+",
+    print "+str(position[1])+"), "+str(zoom)+", ("+str(dimensions[0])+", "+str(dimensions[1])+") )"
+        
+    cpuTime = call_utils.callCPU(position,zoom,dimensions,name,iterations,save=save)
+    cudaTime = call_utils.callCUDA(position,zoom,dimensions,name,iterations,save=save)
 
-	cpuTime = call_utils.callCPU(position,zoom,dimensions,name,iterations,save=save)
-	cudaTime = call_utils.callCUDA(position,zoom,dimensions,name,iterations,save=save)
-
-	print "CPU ran in "+str(cpuTime)+"s"
-	print "CUDA ran in "+str(cudaTime)+"s"
+    print "CPU ran in "+str(cpuTime)+"s"
+    print "CUDA ran in "+str(cudaTime)+"s"
 
 def runComparison():
 
-	execData = {'blocks':range(1,2049),
-		    'threads':range(1,1025)}
+    execData = {'blocks':range(1,2049),
+                'threads':range(1,1025)}
 	
-	position = [-1.3,0]
-	dimensions = [2000,1000]
-	zoom = 900
+    position = [-1.3,0]
+    dimensions = [2000,1000]
+    zoom = 900
 
-	for mode in range(0,5):
-		print "Mode "+str(mode)+":"
-		fractal_data.cudaCollect(position,zoom,dimensions,execData,mode=mode)
-
-#	print "Inserted into index: "+str(index)
-#	data = fractal_data.extractCols(index)
-#	print "len cores,times,threads ("+str(len(cores))+", "+str(len(times))+", "+str(len(threads))+")."
-#	plot_data.makePlot(data,"/home/jshearer/ParallelProjects/graphs/fractal/")
-
-runComparison()
+    for mode in range(0,5):
+    print "Mode "+str(mode)+":"
+    fractal_data.cudaCollect(position,zoom,dimensions,execData,mode=mode)
+    
+    #	print "Inserted into index: "+str(index)
+    #	data = fractal_data.extractCols(index)
+    #	print "len cores,times,threads ("+str(len(cores))+", "+str(len(times))+", "+str(len(threads))+")."
+    #	plot_data.makePlot(data,"/home/jshearer/ParallelProjects/graphs/fractal/")
+    
+if __name__ == '__main__':
+    runComparison()
--- fractal/plot_data.py
+++ fractal/plot_data.py
@@ -1,5 +1,11 @@
+from os import environ
+if environ.has_key('DISPLAY'):
+	ren='WXAgg'
+else:
+	ren = 'Agg'  # Agg to use savefig without DISPLAY set
+
 import matplotlib
-matplotlib.use('Agg') #to use savefig without DISPLAY set
+matplotlib.use(ren)
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter, LogFormatter
 from matplotlib import gridspec
@@ -7,111 +13,116 @@ import matplotlib as pltlib
 import time
 import numpy as np
 
+_VERBOSE = False
+
 def makePlot(data,directory,xlog=True,ylog=False,ovlog=False, show=False):
-	cores      = data[0]
-	times      = data[1]
-	threads    = data[2]
-	zoom       = data[3]
-	mode       = data[4]
-	dimensions = data[5]
-	iterations = data[6]
-	index 	   = data[7]
+    cores      = data[0]
+    times      = data[1]
+    threads    = data[2]
+    zoom       = data[3]
+    mode       = data[4]
+    dimensions = data[5]
+    iterations = data[6]
+    index      = data[7]
 
-	threads_max = max(threads)
-	threads_min = min(threads)
+    threads_max = max(threads)
+    threads_min = min(threads)
 ###############################################
-	threads = np.array(threads,dtype=np.float)
-	cores = np.array(cores,dtype=np.float)
-	times = np.array(times,dtype=np.float)
-	if mode==4:
-		overlap = np.array(data[8],dtype=np.float)
+    threads = np.array(threads,dtype=np.float)
+    cores = np.array(cores,dtype=np.float)
+    times = np.array(times,dtype=np.float)
+    if mode==4:
+        overlap = np.array(data[8],dtype=np.float)
 ###############################################
-#	threads = threads / threads.max()
-#	threads = np.log10(threads)
+#   threads = threads / threads.max()
+#   threads = np.log10(threads)
 ###############################################
-	#times = times / times.max()
-#	times = np.log10(times)
+    #times = times / times.max()
+#   times = np.log10(times)
 ###############################################
-#	cores = np.log10(cores)
+#   cores = np.log10(cores)
 ##############################################
-	print threads_max
+    if _VERBOSE:
+        print threads_max
+        #threads = [(x,x,x) for x in threads]
+        print threads
+        print cores
+        print times
 
-	#threads = [(x,x,x) for x in threads]
-	print threads
-	print cores
-	print times
+    #plt.plot(x_coords,y_coords,'ro')
 
-	#plt.plot(x_coords,y_coords,'ro')
+    x_axis = cores
+    y_axis = times
+    
+    fig = plt.figure(figsize = (12,7)) # dpi=350
 
-	x_axis = cores
-	y_axis = times
-	
-	fig = plt.figure(figsize = (12,7),dpi=350)
+    gspec = gridspec.GridSpec(1,3,width_ratios=[30,4,1])
 
-	gspec = gridspec.GridSpec(1,3,width_ratios=[30,4,1])
+    ax = plt.subplot(gspec[0])
 
-	ax = plt.subplot(gspec[0])
+    #ax.set_xlim([-(x_axis.min()/10.0),x_axis.max()+(x_axis.max()/10.0)])
+    #ax.set_ylim([-(y_axis.min()/10.0),y_axis.max()+(y_axis.max()/10.0)])
 
-	#ax.set_xlim([-(x_axis.min()/10.0),x_axis.max()+(x_axis.max()/10.0)])
-	#ax.set_ylim([-(y_axis.min()/10.0),y_axis.max()+(y_axis.max()/10.0)])
+    sc = ax.scatter(x_axis,y_axis,c=threads,marker="+",norm=pltlib.colors.LogNorm())    
+    cb_ax = plt.subplot(gspec[2])
 
-	sc = ax.scatter(x_axis,y_axis,c=threads,marker="+",norm=pltlib.colors.LogNorm())	
-	cb_ax = plt.subplot(gspec[2])
+    cbar = fig.colorbar(sc, cax=cb_ax)
+    cbar.set_label("Number of threads per core")
 
-	cbar = fig.colorbar(sc, cax=cb_ax)
-	cbar.set_label("Number of threads per core")
+    if mode==4:
+        ax2 = ax.twinx()
+        ax2.ticklabel_format(style="sci",scilimits=(0,0),useOffset=False)
+        print overlap.min(), overlap.max()
+        ov = ax2.scatter(x_axis,overlap,c="k",marker="o",alpha=0.2)
+        ax2.set_ylabel("Overlap (# of pixels)")
+        ax2.set_ylim(overlap.min(),overlap.max())
+        ax.legend((sc,ov),('Time','Overlap'),scatterpoints=4,loc="upper right")
+        pass
+    else:
+        ax.legend((sc,), ('Time',),scatterpoints=4,loc="upper right")
+    
+    ax.set_ylabel("Time to compute "+("log10" if ylog else "")+"(seconds)")
+    ax.set_xlabel("Number of CUDA cores"+(", log10" if xlog else ""))
 
-	if mode==4:
-		ax2 = ax.twinx()
-		ax2.ticklabel_format(style="sci",scilimits=(0,0),useOffset=False)
-		print overlap.min(), overlap.max()
-		ov = ax2.scatter(x_axis,overlap,c="k",marker="o",alpha=0.2)
-		ax2.set_ylabel("Overlap (# of pixels)")
-		ax2.set_ylim(overlap.min(),overlap.max())
-		ax.legend((sc,ov),('Time','Overlap'),scatterpoints=4,loc="upper right")
-		pass
-	else:
-		ax.legend((sc,), ('Time',),scatterpoints=4,loc="upper right")
-	
-	ax.set_ylabel("Time to compute "+("log10" if ylog else "")+"(seconds)")
-	ax.set_xlabel("Number of CUDA cores"+(", log10" if xlog else ""))
+    if xlog:
+        ax.set_xscale('log')
+        if mode==4 and ovlog:
+            ax2.set_xscale('log')
+    if ylog:
+        ax.set_yscale('log')
+        if mode==4 and ovlog:
+            ax2.set_yscale('log')
+    ax.grid(True)
+    ax.axis('tight')
+    title_identifier = {0:'write',1:'read and write',2:'raw compute',3:'atomicAdd test + regular write',4:'Overlap'}[mode]
 
-	if xlog:
-		ax.set_xscale('log')
-		if mode==4 and ovlog:
-			ax2.set_xscale('log')
-	if ylog:
-		ax.set_yscale('log')
-		if mode==4 and ovlog:
-			ax2.set_yscale('log')
-	ax.grid(True)
-	ax.axis('tight')
-	title_identifier = {0:'write',1:'read and write',2:'raw compute',3:'atomicAdd test + regular write',4:'Overlap'}[mode]
+    fig.suptitle("Fractal generation ["+title_identifier+"]\nDimensions: "+str(dimensions)+"\nZoom: "+str(zoom))
+    plt.tight_layout()
+    
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
 
-	fig.suptitle("Fractal generation ["+title_identifier+"]\nDimensions: "+str(dimensions)+"\nZoom: "+str(zoom))
-	plt.tight_layout()
-	
-	plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
-	if show: plt.show()  # doesn't work with Agg???
-	
-	print "Saving file now."
-	fn = directory+"mode_"+str(mode)+"-id"+str(index)+".png"
-	with open(fn,'w') as f:
-		print 'Saving to ', fn
-		plt.savefig(f)
-	plt.clf()
+    print "Saving file now."
+    fn = directory+"mode_"+str(mode)+"-id"+str(index)+".png"
+    with open(fn,'w') as f:
+        print 'Saving to ', fn
+        plt.savefig(f, figsize = (12,7)) 
+
+    # http://stackoverflow.com/questions/9012487/matplotlib-pyplot-savefig-outputs-blank-image
+    if show: plt.show()  # doesn't work with Agg???
+
+    plt.clf()
 
 if __name__ == '__main__':
-	import fractal_data
-	from sys import  argv
-#	print "Inserted into index: "+str(index)
-	if len(argv) == 2:
-		num = argv[1]
-	else:
-		num = 1
+    import fractal_data
+    from sys import  argv
+#   print "Inserted into index: "+str(index)
+    if len(argv) == 2:
+        num = argv[1]
+    else:
+        num = 1
 
-	print 'Displaying index = %s'%num
-	data = fractal_data.extractCols(num)
-#	print "len cores,times,threads ("+str(len(cores))+", "+str(len(times))+", "+str(len(threads))+")."
-	makePlot(data,"results/",ylog=True, show=True)
-	
+    print 'Displaying index = %s'%num
+    data = fractal_data.extractCols(num)
+#   print "len cores,times,threads ("+str(len(cores))+", "+str(len(times))+", "+str(len(threads))+")."
+    makePlot(data,"results/",ylog=True, show=True)
+    
