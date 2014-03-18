923f0191f7fec3f33d4be40110c65648426f9839
Les Schaffer <les@designspring.com>
2014-03-18 00:35:50 -0400

    fix VersionInfo. get alreadyRan, extractMetaData,  and extractCols working in new pair-table scheme. test

--- fractal/fractal_data.py
+++ fractal/fractal_data.py
@@ -40,7 +40,7 @@ class MetaData(tab.IsDescription):
     mode              = tab.UInt8Col()  # should be a string for easier comprehension. 
     iterations        = tab.Int32Col()
 
-    VersionInfo()
+    versioninfo = VersionInfo()
 
 class PairedDataStorage(object):
     @classmethod
@@ -135,70 +135,67 @@ def alreadyRan(position,dimensions,zoom,mode):
     global _data_file
     _init()
 
-    foundL = []
     # isnt there a public func for _f_list_nodes()???
     # TODO: can go straight to the right node eventually and search ROWS of meta data
-    nodeL = _data_file.root.execSets._f_list_nodes()
+    mtable = _data_file.root.TimingData.meta
     
     tupL = (dimensions[0], dimensions[1], mode, position[0], position[1], zoom)
     # NOTE!!!!: the condx string very sensitive. when i replaced a '&' with 'and', all rows were taken!!!
     condS = "(dimensions_x==%d) & (dimensions_y==%d) & (mode == %d) & (pos_x == %f) & (pos_y == %f) & (zoom == %f)"%tupL
     print condS
-    for node in nodeL:
-        # node.meta is a table, search it
-        for row in node.meta.where(condS):
-            print row['dimensions_x'], row['dimensions_y'], row['mode'],  row['pos_x'], row['pos_y'],  row['zoom']
-            foundL.append(node)
-            # we need only know that at least one row in the metadata meets condx
-            # TODO: later we can clean this up when we clean up the table structure
-            break 
+    rowL = [row for row in mtable.where(condS)]
+    if len(rowL) == 0:
+        return None
+    if 0:
+        row = rowL[0]
+        print row['dimensions_x'], row['dimensions_y'], row['mode'],  row['pos_x'], row['pos_y'],  row['zoom']
         
-    return foundL
+    return rowL[0]['index']
 
 def extractCols(nExec):
     global _data_file
     _init()
 
-    meta = _data_file.getNode("/execSets/"+str(nExec)+"/meta")
+    meta = _data_file.root.TimingData.meta
+    data = _data_file.root.TimingData.data
+
+    
+    rowL = [row for row in  meta.where('index == %d'%nExec)]
+    if len(rowL)==0:
+        raise ValueError('no such data items: %d'%nExec)
+    
+    # grab first row data
+    row=rowL[0]
 
     blocks  = []
     times   = []
     threads = []
     overlap = []
     
-    data = _data_file.getNode("/execSets/"+str(nExec)+"/data")
-
-    for execution in data:
+    for execution in data.where('metaIndexFK == %d'%nExec):
         blocks.append(execution['blocks'])
         threads.append(execution['threads'])
         times.append(execution['time'])
-        if meta[0]['mode']==4:
-            overlap.append(execution['overlap'])
+        overlap.append(execution['overlap'])
 
-    iters = 0
-    if 'iterations' in meta[0]:
-        iters = meta[0]['iterations']
-
-    return (blocks,times,threads,meta[0]['zoom'],meta[0]['mode'],
-            (meta[0]['dimensions_x'],meta[0]['dimensions_y']),iters,nExec,overlap)
+    # TODO: should accept, and return dicts!
+    return (blocks,times,threads,row['zoom'],row['mode'],
+            (row['dimensions_x'],row['dimensions_y']), row['iterations'],nExec,overlap)
 
 def extractMetaData():
     global _data_file    
     _init()
 
-    nameL = [ e._v_name for e in _data_file.root.execSets ]
-
     metaD={}
-    for name in nameL:
-        meta = _data_file.getNode("/execSets/"+name+"/meta")
-        metaD[name] = { key:meta[0][key] for key in meta.colnames}
+    for meta in _data_file.root.TimingData.meta:
+        metaD[name] = { key:meta[key] for key in meta.colnames}
 
     return metaD
     
 def getGroup():
     global _data_file
     _init()
-    return _data_file.root.execSets
+    return _data_file.root.TimingData
 
 def _init():
     global _data_file
@@ -206,9 +203,11 @@ def _init():
     if _data_file == None:
         filename = "fractalData.h5"
         _data_file = tab.openFile(filename,mode='a',title="Fractal timing data")
-        if not ("/execSets" in _data_file):
-            desc = "Sets of execution with varying position,zoom,dimensions,blockData, or threadData"
-            _data_file.createGroup("/","execSets",desc)
+        if not ("/TimingData" in _data_file):
+            grp = newFile.create_group("/", 'TimingData', 'Timing data for parallel execution')
+            table = newFile.create_table('/TimingData', 'meta', MetaData, 'Fractal timing meta data')
+            table = newFile.create_table('/TimingData', 'data', ExecutionData, 'Fractal timing data')        
+
 
 if __name__ == '__main__':
 
@@ -216,13 +215,13 @@ if __name__ == '__main__':
         print extractMetaData()
 
     if 1:
-        newFile = tab.openFile('newFractalData.h5', mode='w', title='New Fractal timing data')
+        newFile = tab.openFile('fractalData.h5', mode='w', title='Fractal timing data')
         grp = newFile.create_group("/", 'TimingData', 'Timing data for parallel execution')
         table = newFile.create_table('/TimingData', 'meta', MetaData, 'Fractal timing meta data')
-        table = newFile.create_table('/TimingData', 'data', Execution, 'Fractal timing data')        
+        table = newFile.create_table('/TimingData', 'data', ExecutionData, 'Fractal timing data')        
 
     if 1:
-        _newFile = tab.openFile('newFractalData.h5', mode='a', title='New Fractal timing data')        
+        _newFile = tab.openFile('fractalData.h5', mode='a', title='New Fractal timing data')        
         theMetaTable = _newFile.root.TimingData.meta
         theDataTable = _newFile.root.TimingData.data   
         row = theMetaTable.row
@@ -235,17 +234,22 @@ if __name__ == '__main__':
             row['pos_x']= 2.1
             row['pos_y']= 1.7
             row['zoom']=900.
+            row['versioninfo/code_git'] = 'brand new'
+            row['versioninfo/nvidia'] = 'brand new'
+            row['versioninfo/cuda'] = 'brand new'
+            row['versioninfo/os'] = 'brand new'
+            row['versioninfo/gcc_python'] = 'brand new'              
             row['index'] = i
             row.append()
             for j in range(3):
                 drow['metaIndexFK'] = i
-                drow['block_x'] = 1000+j
-                drow['block_y'] = 1000+j
-                drow['blocks'] = 1000+j
-                drow['thread_x'] = 1000+j
-                drow['thread_y'] = 1000+j
-                drow['threads'] = 1000+j
-                drow['time'] = 2.1+float(j)/3
+                drow['block_x'] = 1000+j+i
+                drow['block_y'] = 1000+j+i
+                drow['blocks'] = 1000+j+i
+                drow['thread_x'] = 1000+j+i
+                drow['thread_y'] = 1000+j+i
+                drow['threads'] = 1000+j+i
+                drow['time'] = 2.1+float(j+i)/3
                 drow.append()
 
 
@@ -253,8 +257,10 @@ if __name__ == '__main__':
         theDataTable.flush()        
         
         
-    if 0:
+    if 1:
         
-        res= alreadyRan( (-1.3,0.0), (2000, 1000), 900., 4)
-        print res
+        res= alreadyRan( (2.1,1.7), (20, 30), 900., 2)
+        if res: print extractCols(res)
 
+        print  extractCols(0)
+        print  extractCols(1)
