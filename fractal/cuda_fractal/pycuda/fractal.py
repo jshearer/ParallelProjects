65a246afaacc745e6ab6ff965e23957410d2c0b1
les <schaffer@optonline.net>
2014-03-24 22:36:07 -0400

    moved CUDA code to genchunk.cu compileCuda() and global _genChunk, compiled as needed.

--- fractal/cuda_fractal/pycuda/fractal.py
+++ fractal/cuda_fractal/pycuda/fractal.py
@@ -16,85 +16,27 @@ import pdb
 from Vector import Vector
 import utils
 
-genChunk = SourceModule("""
-#include <cuComplex.h>
-
-__device__ int map2Dto1D(int x, int y,int x_width)
-{
-	return x+(y*x_width);	
-}
-
-__global__ void gen(int px_per_block[2],int px_per_thread[2],int size[2],float position[2],float *zoom,int *iterations,int *result, int* progress,int action)
-{ 	//blockDim = size of threads per block
-	//gridDim = size of blocks
-
-	//int size[2] argument is just to make sure we don't fall off the edge and crash the entire machine...
-
-	//actions: 0 = write
-	//	   1 = read+write
-	//     2 = none	
-	//	   3 = atomicAddTest
-	//	   4 = overlapMap
-
-	int startx = (blockIdx.x*px_per_block[0])+(threadIdx.x*px_per_thread[0]);
-	int starty = (blockIdx.y*px_per_block[1])+(threadIdx.y*px_per_thread[1]);
-
-	float t_x, t_y;
-	int i, x, y;
-
-	cuFloatComplex z = cuFloatComplex();
-	cuFloatComplex z_unchanging = cuFloatComplex();
-
-	float z_real, z_imag;
-
-	for(x = startx; x < startx+px_per_thread[0]; x++){
-		for(y = starty; y < starty+px_per_thread[1]; y++){
-			if(action==3)
-			{
-				atomicAdd(progress,1);
-			}
-			t_x = (x+position[0])/(*zoom);
-			t_y = (y+position[1])/(*zoom);
-
-			z.x = t_x;
-			z.y = t_y;
-			z_unchanging.x = t_x;
-			z_unchanging.y = t_y; //optomize this with pointer magic?
-			if(action==4) //generate overlap map
-			{
-				result[map2Dto1D(x,y,size[0])] = result[map2Dto1D(x,y,size[0])] + 1;
-			} else
-			{
-				for(i = 0; i<(*iterations) + 1; i++){
-					z = cuCmulf(z,z);
-					z = cuCaddf(z,z_unchanging); //z = z^2 + z_orig
-					z_real = cuCrealf(z);
-					z_imag = cuCimagf(z);
-					if((z_real*z_real + z_imag*z_imag)>4){
-						if(action==0)//act cool, do the default
-						{
-							result[map2Dto1D(x,y,size[0])] = i;
-						} else if(action==1)// read+write test
-						{
-							result[map2Dto1D(x,y,size[0])] = result[map2Dto1D(x,y,size[0])] + 1;
-						}//else if action==2, do nothing
-						break;
-					}
-				}
-			}
-		}
-	}
-}
-""").get_function("gen")
-print("Compiled and got function gen")
-
 def In(thing):
 	thing_pointer = cuda.mem_alloc(thing.nbytes)
 	cuda.memcpy_htod(thing_pointer, thing)
 	return thing_pointer
 
+_genChunk=None
+def compileCuda(srcN='cuda_fractal/pycuda/genchunk.cu'):
+        global _genChunk
+        _fp = open(srcN, 'r')
+        _genSrc = _fp.read()
+        _fp.close()
+        
+        _genChunk = SourceModule(_genSrc).get_function("gen")
+        print("Compiled and got function gen")
+
 def GenerateFractal(dimensions,position,zoom,iterations,scale=1,action=0,block=(15,15,1),thread=(1,1,1),
                     report=False, silent=False, debug=True,px_per_block=False,px_per_thread=False):
+        global _genChunk
+
+        if not _genChunk: compileCuda()
+
 	#Force progress checking to False, otherwise it'll go on forever with reporting turned off in the kernel
 	report = False
 	zoom = zoom * scale
@@ -169,7 +111,7 @@ def GenerateFractal(dimensions,position,zoom,iterations,scale=1,action=0,block=(
 	end = cuda.Event()
 	start.record()
 	
-	genChunk(blockpx, threadpx, dims, posit, zoo, iters, res, ppc_ptr, act, block=thread, grid=block)
+	_genChunk(blockpx, threadpx, dims, posit, zoo, iters, res, ppc_ptr, act, block=thread, grid=block)
 	
 	end.record()
 	cuda.Context.synchronize()
---
+++ fractal/cuda_fractal/pycuda/genchunk.cu
@@ -0,0 +1,70 @@
+#include <cuComplex.h>
+
+__device__ int map2Dto1D(int x, int y,int x_width)
+{
+  return x+(y*x_width);	
+}
+
+__global__ void gen(int px_per_block[2],int px_per_thread[2],int size[2],float position[2],float *zoom,
+		    int *iterations,int *result, int* progress,int action)
+{ 	
+  //blockDim = size of threads per block
+  //gridDim = size of blocks
+  
+  //int size[2] argument is just to make sure we don't fall off the edge and crash the entire machine...
+  
+  //actions: 0 = write
+  //	     1 = read+write
+  //         2 = none	
+  //	     3 = atomicAddTest
+  //	     4 = overlapMap
+  
+  int startx = (blockIdx.x*px_per_block[0])+(threadIdx.x*px_per_thread[0]);
+  int starty = (blockIdx.y*px_per_block[1])+(threadIdx.y*px_per_thread[1]);
+  
+  float t_x, t_y;
+  int i, x, y;
+  
+  cuFloatComplex z = cuFloatComplex();
+  cuFloatComplex z_unchanging = cuFloatComplex();
+  
+  float z_real, z_imag;
+  
+  for(x = startx; x < startx+px_per_thread[0]; x++){
+    for(y = starty; y < starty+px_per_thread[1]; y++){
+      if(action==3)
+	{
+	  atomicAdd(progress,1);
+	}
+      t_x = (x+position[0])/(*zoom);
+      t_y = (y+position[1])/(*zoom);
+      
+      z.x = t_x;
+      z.y = t_y;
+      z_unchanging.x = t_x;
+      z_unchanging.y = t_y; //optomize this with pointer magic?
+      if(action==4) //generate overlap map
+	{
+	  result[map2Dto1D(x,y,size[0])] = result[map2Dto1D(x,y,size[0])] + 1;
+	} else
+	{
+	  for(i = 0; i<(*iterations) + 1; i++) {
+	    z = cuCmulf(z,z);
+	    z = cuCaddf(z,z_unchanging); //z = z^2 + z_orig
+	    z_real = cuCrealf(z);
+	    z_imag = cuCimagf(z);
+	    if((z_real*z_real + z_imag*z_imag)>4){
+	      if(action==0)//act cool, do the default
+		{
+		  result[map2Dto1D(x,y,size[0])] = i;
+		} else if(action==1)// read+write test
+		{
+		  result[map2Dto1D(x,y,size[0])] = result[map2Dto1D(x,y,size[0])] + 1;
+		}//else if action==2, do nothing
+	      break;
+	    }
+	  }
+	}
+    }
+  }
+}
