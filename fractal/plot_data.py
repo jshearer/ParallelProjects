19a635f777f115d7852e80908835065b1b541871
Les Schaffer <les@designspring.com>
2014-03-26 14:28:42 -0400

    refactored makePlot, pulled out a _create_plot that loop_queue_plot, makePlot, and others could use. mostly tested, need to check overlap code.

--- fractal/plot_data.py
+++ fractal/plot_data.py
@@ -14,25 +14,16 @@ import time
 import numpy as np
 
 _VERBOSE = False
+_modeD = {0:'write',1:'read/write',2:'raw compute',3:'atomicAdd test + write',4:'overlap'}
 
-def loop_queue_plot(timeL, blocksL, pptL, dimx, dimy, threads, mode, xaxis, save=False, show=True):
-    xlog=True
-    ylog=False
-    times = np.array(timeL,dtype=np.float)
-    blocks = np.array(blocksL,dtype=np.float)
-    ppt = np.array(pptL,dtype=np.float)
+def _create_plot(axisT, mode, title, legendT, show, save, fn):
+    """2D plot, with right hand colorbar, and optional overlay for another curve"""
+    x_axis, xlabel, xlog = axisT[0]
+    y_axis, ylabel, ylog = axisT[1]
+    c_axis, clabel = axisT[2]
+    if mode==4:
+        o_axis, olabel, olog = axisT[3]
 
-    if xaxis == 'px_per_thread':
-        x_axis = ppt
-        c_axis = blocks
-        clabel = 'Blocks'
-    elif xaxis == 'blocks':
-        x_axis = blocks
-        c_axis = ppt
-        clabel = 'px_per_thread'
-        
-    y_axis = times
-    
     fig = plt.figure(figsize = (12,7)) # dpi=350
 
     gspec = gridspec.GridSpec(1,3,width_ratios=[30,4,1])
@@ -45,24 +36,36 @@ def loop_queue_plot(timeL, blocksL, pptL, dimx, dimy, threads, mode, xaxis, save
     cbar = fig.colorbar(sc, cax=cb_ax)
     cbar.set_label(clabel)
 
-    ax.legend((sc,), ('Time',),scatterpoints=4,loc="upper center")
-    
-    ax.set_ylabel("Time to compute "+("log10" if ylog else "")+"(seconds)")
-    ax.set_xlabel("Number of CUDA %s"%xaxis+(", log10" if xlog else ""))
+    if mode==4:
+        ax2 = ax.twinx()
+        ax2.ticklabel_format(style="sci",scilimits=(0,0),useOffset=False)
+        ov = ax2.scatter(x_axis,o_axis,c="k",marker="o",alpha=0.2)
+        ax2.set_ylabel(olabel)
+        ax2.set_ylim(o_axis.min(),o_axis.max())
+        ax.legend((sc,ov),legenT,scatterpoints=4,loc="upper right")
+    else:
+        ax.legend((sc,), legendT,scatterpoints=4,loc="upper center")
 
-    ax.set_xscale('log')
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    
+    if xlog:
+        ax.set_xscale('log')
+        if mode==4 and olog:
+            ax2.set_xscale('log')
+    if ylog:
+        ax.set_yscale('log')
+        if mode==4 and olog:
+            ax2.set_yscale('log')
     ax.grid(True)
     ax.axis('tight')
-    modeS = {0:'write',1:'read and write',2:'raw compute',
-	     3:'atomicAdd test + regular write',4:'Overlap'}[mode]
 
-    fig.suptitle("Queue vs looping: pixels=(%d, %d) #threads = %d  mode=%s"%(dimx, dimy, threads, modeS))
+    fig.suptitle(title)
     plt.tight_layout()
     
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
 
     if save:
-        fn = directory+"mode_"+str(mode)+"-id"+str(index)+".png"
         with open(fn,'w') as f:
             print 'Saving to ', fn
             plt.savefig(f, figsize = (12,7)) 
@@ -71,7 +74,35 @@ def loop_queue_plot(timeL, blocksL, pptL, dimx, dimy, threads, mode, xaxis, save
     if show: plt.show()  # do nothing if Agg
 
     plt.clf()
-	
+
+
+def loop_queue_plot(timeL, blocksL, pptL, dimx, dimy, threads, mode, xaxis, idS='X', save=False, show=True):
+    y_axis = np.array(timeL,dtype=np.float)
+    blocks = np.array(blocksL,dtype=np.float)
+    ppt = np.array(pptL,dtype=np.float)
+
+    if xaxis == 'px_per_thread':
+        x_axis = ppt
+        c_axis = blocks
+        clabel = 'Blocks'
+    elif xaxis == 'blocks':
+        x_axis = blocks
+        c_axis = ppt
+        clabel = 'px_per_thread'
+
+    xlog=True
+    ylog=False
+
+    xlabel="Number of CUDA %s"%xaxis+(", log10" if xlog else "")
+    ylabel="Time to compute "+("log10" if ylog else "")+"(seconds)"
+
+    title = "Queue vs looping: pixels=(%d, %d) #threads = %d  mode=%s"%(dimx, dimy, threads, _modeD[mode])
+    fn = 'loop-queue-%s-%s'%(xaxis, idS)
+
+    axisT = ( (x_axis,xlabel,xlog), (y_axis, ylabel, ylog), (c_axis, clabel) )
+    
+    _create_plot(axisT, mode, title, ('Time', ), show=show, save=save, fn=fn)
+
 
 def makePlot(data,directory,xaxis='cores',xlog=True,ylog=False,ovlog=False,show=False,save=True):
     cores      = data[0]
@@ -88,10 +119,11 @@ def makePlot(data,directory,xaxis='cores',xlog=True,ylog=False,ovlog=False,show=
 ###############################################
     threads = np.array(threads,dtype=np.float)
     cores = np.array(cores,dtype=np.float)
-    times = np.array(times,dtype=np.float)
+    y_axis = np.array(times,dtype=np.float)
     dimensions = np.array(dimensions, dtype=np.int)
+    o_axis=None
     if mode==4:
-        overlap = np.array(data[8],dtype=np.float)
+        o_axis = np.array(data[8],dtype=np.float)
 ###############################################
 #   threads = threads / threads.max()
 #   threads = np.log10(threads)
@@ -126,68 +158,24 @@ def makePlot(data,directory,xaxis='cores',xlog=True,ylog=False,ovlog=False,show=
         x_axis = dimensions.prod() / cores / threads
         c_axis = cores
         clabel = 'Blocks'
+
+    olabel = "Overlap (# of pixels)"
+    legendT = ('Time', )
+    if mode == 4:
+        legendT.append('Overlap')
         
-    y_axis = times
+    xlabel="Number of CUDA %s"%xaxis+(", log10" if xlog else "")
+    ylabel="Time to compute "+("log10" if ylog else "")+"(seconds)"
+
+    modeS = _modeD[mode]
+
+    title="Fractal generation ["+modeS+"]\nDimensions: "+str(dimensions)+"\nZoom: "+str(zoom) 
+    fn = directory+"mode_"+modeS+"-id"+str(index)+".png"
+
+    axisT = ( (x_axis,xlabel,xlog), (y_axis, ylabel, ylog), (c_axis, clabel), (o_axis, olabel, ovlog) )
+
+    _create_plot(axisT, mode, title, legendT, show=show, save=save, fn=fn)
     
-    fig = plt.figure(figsize = (12,7)) # dpi=350
-
-    gspec = gridspec.GridSpec(1,3,width_ratios=[30,4,1])
-
-    ax = plt.subplot(gspec[0])
-
-    #ax.set_xlim([-(x_axis.min()/10.0),x_axis.max()+(x_axis.max()/10.0)])
-    #ax.set_ylim([-(y_axis.min()/10.0),y_axis.max()+(y_axis.max()/10.0)])
-
-    sc = ax.scatter(x_axis,y_axis,c=c_axis,marker="+",norm=pltlib.colors.LogNorm())    
-    cb_ax = plt.subplot(gspec[2])
-
-    cbar = fig.colorbar(sc, cax=cb_ax)
-    cbar.set_label(clabel)
-
-    if mode==4:
-        ax2 = ax.twinx()
-        ax2.ticklabel_format(style="sci",scilimits=(0,0),useOffset=False)
-        print overlap.min(), overlap.max()
-        ov = ax2.scatter(x_axis,overlap,c="k",marker="o",alpha=0.2)
-        ax2.set_ylabel("Overlap (# of pixels)")
-        ax2.set_ylim(overlap.min(),overlap.max())
-        ax.legend((sc,ov),('Time','Overlap'),scatterpoints=4,loc="upper right")
-        pass
-    else:
-        ax.legend((sc,), ('Time',),scatterpoints=4,loc="upper center")
-    
-    ax.set_ylabel("Time to compute "+("log10" if ylog else "")+"(seconds)")
-    ax.set_xlabel("Number of CUDA %s"%xaxis+(", log10" if xlog else ""))
-
-    if xlog:
-        ax.set_xscale('log')
-        if mode==4 and ovlog:
-            ax2.set_xscale('log')
-    if ylog:
-        ax.set_yscale('log')
-        if mode==4 and ovlog:
-            ax2.set_yscale('log')
-    ax.grid(True)
-    ax.axis('tight')
-    title_identifier = {0:'write',1:'read and write',2:'raw compute',
-                        3:'atomicAdd test + regular write',4:'Overlap'}[mode]
-
-    fig.suptitle("Fractal generation ["+title_identifier+"]\nDimensions: "+str(dimensions)+"\nZoom: "+str(zoom))
-    plt.tight_layout()
-    
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
-
-    if save:
-        fn = directory+"mode_"+str(mode)+"-id"+str(index)+".png"
-        with open(fn,'w') as f:
-            print 'Saving to ', fn
-            plt.savefig(f, figsize = (12,7)) 
-
-    # http://stackoverflow.com/questions/9012487/matplotlib-pyplot-savefig-outputs-blank-image
-    if show: plt.show()  # do nothing if Agg
-
-    plt.clf()
-
 def _graph(arguments):
 
     if not (arguments.show or arguments.save):
